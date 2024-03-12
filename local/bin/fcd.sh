2ea30c2b94767b06d9a85a8576d9c4a0aeb6a9cb
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-03-12 08:39:42 +0800

    feat: add '..' to return to parent dir

--- local/bin/fcd.sh
+++ local/bin/fcd.sh
@@ -2,15 +2,17 @@
 
 # Somthing with cd being built-in function that doesn't play alone will with using a alias, which will cause only the subshell to be cd not the top one, useless. Using source and a function to cd the top shell
 
-fcd(){
-    dir=$1
-    content=$(fd -H . -d 1 -t d -t l | sort -r)
-    [[ -z $content ]] && return
+fcd() {
+	dir=$1
+	content=$(fd -H . -d 1 -t d -t l | sort -r)
+	content="$content\n..\n."
 
-    dir=$(echo "$content" | fzf --height 50% --preview 'exa -TF -L=1 {}' --preview-window=right:40% --preview-label $(pwd) --preview-label-pos=bottom)
-    if [[ -n "$dir" ]]; then
-        cd "$dir" || exit
-        fcd "$dir"
-    fi
+	[[ -z $content ]] && return
+
+	dir=$(echo "$content" | fzf --height 50% --preview 'exa -TF -L=1 {}' --preview-window=right:40% --preview-label $(pwd) --preview-label-pos=bottom)
+
+	if [[ -n "$dir" ]]; then
+		cd "$dir" || exit
+		fcd "$dir"
+	fi
 }
-
