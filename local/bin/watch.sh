f464da7f4329972e78a9869a66faa1abf1f612b4
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-05 14:00:36 +0800

    feat: add watch.sh

---
+++ local/bin/watch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Path to the script you want to watch
+[[ -z "$1" ]] && echo "usage: watch file" && exit 1
+script="$1"
+
+# Function to execute the script
+execute_script() {
+	echo "Changes detected. Executing $script..."
+	bash "$script"
+}
+
+# Execute the script once initially
+execute_script
+
+# Watch for changes and execute the script
+while true; do
+	fswatch -1 "$script" >/dev/null
+	execute_script
+done
