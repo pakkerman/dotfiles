9a189abf911f02bc66c39ea7fa3e5e7264d47bce
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-10 09:16:44 +0800

    feat: add dirs

--- local/bin/tmux-sessionizer.sh
+++ local/bin/tmux-sessionizer.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-session=$(find ~/Dropbox/Coding ~/temp -maxdepth 1 -mindepth 1 -type d | fzf)
+dirs=("$HOME/Dropbox/Coding" "$HOME/temp" "$HOME/Dropbox/Coding/learning/" "$HOME/scripts/")
+session=$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d | fzf --margin 10% --border=bold --border-label="Projector Finder" --border-label-pos=bottom)
 session_name=$(basename "$session" | tr '.' '_')
 
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
-  tmux new-session -s "$session_name" -c "$session" -d
+if ! tmux has-session -t "$session_name" 2>/dev/null; then
+	tmux new-session -s "$session_name" -c "$session" -d
 fi
 
 tmux switch-client -t "$session_name"
- 
