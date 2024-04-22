e3f37bdb2fbdf9a38acb82cc04e9c17001187ad5
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-22 19:03:25 +0800

    feat: open nvim and a new window

--- local/bin/tmux-sessionizer.sh
+++ local/bin/tmux-sessionizer.sh
@@ -9,7 +9,7 @@ session=$(echo -e "$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d)" "$other
 session_name=$(basename "$session" | tr '.' '_')
 
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
-	tmux new-session -s "$session_name" -c "$session" -d
+	tmux new-session -s "$session_name" -c "$session" -d "tmux new-window -d; nvim"
 fi
 
 tmux switch-client -t "$session_name"
