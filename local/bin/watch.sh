ad4410c99553731c3f2fc261df1b4952a1a887d6
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-05-07 08:30:44 +0800

    feat: add args

--- local/bin/watch.sh
+++ local/bin/watch.sh
@@ -3,12 +3,13 @@
 # Path to the script you want to watch
 [[ -z "$1" ]] && echo "usage: watch file" && exit 1
 script="$1"
+arg="$2"
 
 # Function to execute the script
 execute_script() {
 	clear
 	echo "Changes detected. Executing $script..."
-	bash "$script"
+	bash "$script $arg"
 }
 
 # Execute the script once initially
@@ -17,5 +18,5 @@ execute_script
 # Watch for changes and execute the script
 while true; do
 	fswatch -1 "$script" >/dev/null
-	execute_script
+	execute_script "$arg"
 done
