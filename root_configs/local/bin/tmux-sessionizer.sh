e17a4b539755d4d75eb3266e1ad5d28511f019ce
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-09-17 08:33:55 +0800

    fix: correctly displaying borders

--- root_configs/local/bin/tmux-sessionizer.sh
+++ root_configs/local/bin/tmux-sessionizer.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 dirs=(
-    "$HOME/Dropbox/Coding"
-    "$HOME/Dropbox/Coding/learning"
-    "$HOME/Dropbox/Coding/Projects"
-    "$HOME/temp"
-    "$HOME/git"
-    "$HOME/scripts"
-    "$HOME/image-generation/stable-diffusion-webui"
+  "$HOME/Dropbox/Coding"
+  "$HOME/Dropbox/Coding/learning"
+  "$HOME/Dropbox/Coding/Projects"
+  "$HOME/temp"
+  "$HOME/git"
+  "$HOME/scripts"
+  "$HOME/image-generation/stable-diffusion-webui"
 )
 
 others="\n$HOME/dotfiles"
 
 session=$(
-    echo -e "$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d)" "$others" |
-        sed "s|$HOME/||g" |
-        fzf --reverse \
-            --margin 10% \
-            --border=bold \
-            --border-label="Project Finder" \
-            --preview "if [ -f $HOME/{}/README.md ]; then bat $HOME/{}/README.md; else echo No README.md found in this directory; fi" \
-            --preview-window bottom
+  echo -e "$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d)" "$others" |
+    sed "s|$HOME/||g" |
+    fzf --reverse \
+      --margin 10% \
+      --border=rounded \
+      --border-label="| Project Finder |" \
+      --preview "if [ -f $HOME/{}/README.md ]; then bat $HOME/{}/README.md; else echo No README.md found in this directory; fi" \
+      --preview-window bottom
 )
 
 [[ -z $session ]] && exit 1
@@ -33,11 +33,11 @@ session_name=$(basename "$session" | tr '.' '_')
 #
 existing_session=$(tmux has-session -t "$session_name" 2>/dev/null)
 if ! "$existing_session"; then
-    tmux new-session \
-        -d \
-        -c "$session" \
-        -s "$session_name" \
-        "tmux new-window -d lazygit; nvim; exec $SHELL"
+  tmux new-session \
+    -d \
+    -c "$session" \
+    -s "$session_name" \
+    "tmux new-window -d lazygit; nvim; exec $SHELL"
 fi
 
 tmux switch-client -t "$session_name"
