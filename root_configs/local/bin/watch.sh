0b4eef25db1d706d40a1316f7b666aa384c2a4ed
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-01-13 17:46:58 +0800

    chore: rewrite

--- root_configs/local/bin/watch.sh
+++ root_configs/local/bin/watch.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-# Path to the script you want to watch
-[[ -z "$1" ]] && echo "usage: watch shell-program file [arg] [watchlist]" && exit 1
-[[ -z "$2" ]] && echo "Missing script file" && exit 1
-program="$1"
-script="$2"
-arg="$3"
-watchlist=("${@:4}")
+if (($# == 0)); then
+  echo "Usage: watch <cmd>" >&2
+  exit 1
+fi
 
-[[ ${#watchlist[@]} -eq 0 ]] && watchlist=("$script")
+watchlist=(*.*)
+echo "watching ${watchlist[*]}"
+
+# [[ ${#watchlist[@]} -eq 0 ]] && watchlist=("$script")
 
 # Function to execute the script
 execute_script() {
   clear
-  echo "file changed, re-running command: $program $script"
-  echo "watchlist: ${watchlist[@]}"
-  "$program" "$script" "$arg"
+  echo "file changed, re-running command: $*"
+  "$@"
 }
 
 cleanup() {
@@ -26,10 +25,10 @@ cleanup() {
 trap cleanup SIGINT
 
 # Execute the script once initially
-execute_script
+execute_script "$@"
 
 # Watch for changes and execute the script
 while true; do
   fswatch -1 "${watchlist[@]}" >/dev/null
-  execute_script
+  execute_script "$@"
 done
